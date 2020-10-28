d4202b1eb3e422c6e1553394d424047ea9873407
mayconbalves <mayconmustaine@hotmail.com>
2020-10-27 22:03:53 -0300

    chore(webpack): add rules css and img

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,67 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    chunkFilename: '[id].js',
+    publicPath: ''
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: 'babel-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.html$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
         use: [
-          {
-            loader: "html-loader"
+          { loader: "html-loader" },
+          { loader: 'style-loader' },
+          { 
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]"
+              },														
+              sourceMap: true
+            }
+          },
+          { 
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [ 'autoprefixer', {}, ]
+                ],
+              },
+            }
           }
         ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=10000&name=img/[name].[ext]'
       }
     ]
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+    new HtmlWebpackPlugin({
+        template: __dirname + '/src/index.html',
+        filename: 'index.html',
+        inject: 'body'
     })
   ]
 }
